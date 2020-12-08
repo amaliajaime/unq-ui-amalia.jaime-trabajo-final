1bcc651776ce3bfd5ebe576d6ddce4345b8e13f2
Amalia Jaime <amaliajaime3@gmail.com>
2020-12-07 23:31:11 -0300

    Se agrega pagina de inicio

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Jugar from './Jugar.jsx';
 
 const Home = () => {
 
@@ -10,14 +11,24 @@ const Home = () => {
         {label:'Select', value:'0'},
         {label:'1 PLayer', value:'1'},
         {label:'2 Players', value:'2'},
-      ]
+    ]
     
-    const handleChange = (event) => {
-        setPlayer(event.target.value)
+    const handleClickPlayer = (event) => {
+        event.preventDefault();
+        setPlayer('1');
+        console.log(player);
+    }
+
+    const handleClickPlayers = (event) => {
+        event.preventDefault();
+        setPlayer('2')
+        console.log(player);
     }
 
     const playGame = () => {
-
+        return(
+        <Jugar user={player} />
+        );
     }
 
     const changeName = (event) => {
@@ -34,9 +45,31 @@ const Home = () => {
     <div>
         <h1>Piedra, Papel, Tijera, Lagarto o Spock</h1>
         <form>
-            <select name={options}  options={options} onChange={handleChange}>
-                {options.map(optionPLayer => <option key={optionPLayer.value} value={optionPLayer.value}>{optionPLayer.label}</option>)}
-            </select>
+        <div>
+            <button onClick={handleClickPlayer} className="image-button margin-top-md">
+                <img src="/player1.png" className=""/>
+            </button>
+
+            <button onClick={handleClickPlayers} className="image-button margin-top-md">
+                <img src="/player2.png" className=""/>
+            </button>
+            <a> Se seleccionaron {player} jugador/es </a>
+        </div>
+
+
+        <div>
+        </div>
+
+
+
+            {player.value === 2 ?
+               <div>
+                    <input value={player1} onChange={changeName} className="form-control mb-2" placeholder="Player 1" />
+                    <input value={player2} onChange={changeName2} className="form-control mb-2" placeholder="Player 2" />
+                </div> :
+                <div>
+                    <input value={player1} onChange={changeName} className="form-control mb-2" placeholder="Player 1" />
+                </div> }
             <button type="button" onClick={playGame}>Jugar</button>
         </form>
     </div>
